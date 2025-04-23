1d4406e8f1468eb34d292fbc501b23ebe411d57e
gabims029 <gabimelo541@gmail.com>
2025-04-23 16:18:48 -0300

    --

--- src/pages/Sala.jsx
+++ src/pages/Sala.jsx
@@ -4,16 +4,56 @@ import Box from "@mui/material/Box";
 import Header from "../components/HeaderLogo";
 import Footer from "../components/Footer";
 
-function Sala() {
-  return (
-    <Box>
-      <Header />
-      <Typography fontSize={30} sx={{ textAlign: "center", marginTop:10 }}>
-        Sala
-      </Typography>
-      <Footer />
-    </Box>
-  );
-}
+function HorariosDisponiveis() {
+  const [horarios, setHorarios] = useState([]);
+  const [alert, setAlert] = useState({
+    open: false, 
+    severity: "", 
+    message: "" 
+  });
 
+  async function getHorarios(){
+    // Chamada da Api
+    await api.getHorarios().then(
+      (response)=>{
+        console.log(response.data.users)
+        setHorarios(response.data.users)
+      },(error)=>{
+        console.log("Erro ",error)
+      }
+    )
+  }
+}
 export default Sala;
+
+
+// <ConfirmDelete
+//       open={modalOpen}
+//       userName={userToDelete.name}
+//       onConfirm={deleteUser}
+//       onClose={()=>setModalOpen(false)}
+//       />
+//       {users.length === 0 ?(<h1>Carregando usuários</h1>): (
+//       <div>
+//         <h5>Lista de usuários</h5>
+//         <TableContainer component={Paper} style={{margin:"2px"}}>
+//           <Table size="small">
+//             <TableHead style={{backgroundColor: "lightskyblue", borderStyle:"solid"}}>
+//               <TableRow>
+//                 <TableCell align="center">
+//                   Nome
+//                 </TableCell>
+//                 <TableCell align="center">
+//                   Email
+//                 </TableCell>
+//                 <TableCell align="center">
+//                   CPF
+//                 </TableCell>
+//                 <TableCell align="center">
+//                   Ações
+//                 </TableCell>
+//               </TableRow>
+//             </TableHead>
+//             <TableBody>{listUsers}</TableBody>
+//           </Table>
+//         </TableContainer>
